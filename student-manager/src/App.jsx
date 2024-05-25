421b6bac35b02ca24e987d56bbcea1f62218ae6c
Cenk Kaynak <cenk.kaynak91@gmail.com>
2024-05-25 12:06:29 +0300

    browser router

--- student-manager/src/App.jsx
+++ student-manager/src/App.jsx
@@ -3,19 +3,28 @@ import StudentForm from './components/student-form/studentForm';
 import StudentList from './components/student-list/studentList';
 import Header from './components/shared/header/header';
 import { StudentProvider } from './contexts/StudentContext';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { LandingPage } from './pages/shared/landing-page/LandingPage';
 
 function App() {
 
   return (
-    <StudentProvider >
-      <Header title={"Student Manager"} navElements={["Home", "Profile", "Contact"]} />
-      <br />
-      <main>
-        <StudentForm />
+
+    <BrowserRouter>
+
+      <StudentProvider >
+        <Header title={"Student Manager"} navElements={["Home", "Profile", "Contact"]} />
         <br />
-        <StudentList  />
-      </main>
-    </StudentProvider>
+        <main>
+          <Routes>
+            <Route path='/' element={<LandingPage />} />
+            <Route path='/students' element={ <StudentList/>} />
+            <Route path='/students/new' element={ <StudentForm/>} />
+          </Routes>
+        </main>
+      </StudentProvider>
+
+    </BrowserRouter>
   )
 }
 
---
+++ student-manager/src/pages/shared/landing-page/LandingPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const LandingPage = () => {
+  return (
+    <h2>Welcome to Student Manager</h2>
+  )
+}
