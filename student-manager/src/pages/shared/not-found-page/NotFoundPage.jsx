51dd55889719c8839d83db716eb96c9945f09c9b
Cenk Kaynak <cenk.kaynak91@gmail.com>
2024-05-25 14:01:36 +0300

    timeout with setInverval and clearInterval

--- student-manager/src/pages/shared/not-found-page/NotFoundPage.jsx
+++ student-manager/src/pages/shared/not-found-page/NotFoundPage.jsx
@@ -8,16 +8,21 @@ const NotFoundPage = () => {
     const [timer, setTimer] = useState(5);
 
     useEffect(() => {
-        const countdown = setTimeout(() => {
+        const countdown = setInterval(() => {
+            //her saniye 1 kere çalışacak bir geri sayım yarattım.
             setTimer(prevTimer => {
+                //prevTimer 0'a azalanana kadar her seferinde prevTimer'ı 1 eksiltecek ve 0 olduğunda home page yönlendirecek.
                 if (prevTimer === 0) {
                     navigate("/");
                 }
                 return prevTimer - 1;
-            })
+            });
+            //1000 ms yani 1 saniye'de bir çalışacak şekilde ayarladım. Bu sayede her bir saniyede prevTimer 1 azalacak.
         }, 1000);
 
-    }, [timer])
+        //clearInterval kullanmadığımda geri sayım hatalı olacağı için 1 saniyede bir saymak yerine saniye atlayabilir.
+        return () => clearInterval(countdown);
+    }, [])
 
     return (
         <div>
